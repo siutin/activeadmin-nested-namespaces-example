563c3ea8f0aa76105fbf60a21b0c0b1f64ecbd41
martin <osiutino@gmail.com>
2018-08-28 13:33:57 +0800

    enable activeadmin nested namespace plugin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180828052043) do
+ActiveRecord::Schema.define(version: 20180828052044) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -41,6 +41,15 @@ ActiveRecord::Schema.define(version: 20180828052043) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "author"
+    t.boolean "is_published"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
