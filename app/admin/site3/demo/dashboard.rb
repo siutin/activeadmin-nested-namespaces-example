6d12aead567ba748ae139f73559aba093b7583e0
martin <osiutino@gmail.com>
2018-08-28 13:35:44 +0800

    create nested namespaces demo

--- app/admin/site3/demo/dashboard.rb
+++ app/admin/site3/demo/dashboard.rb
@@ -1,11 +1,11 @@
-ActiveAdmin.register_page "Dashboard" do
+ActiveAdmin.register_page "Dashboard", namespace: [:site3, :demo] do
 
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
     div class: "blank_slate_container", id: "dashboard_default_message" do
       span class: "blank_slate" do
-        h1 "default"
+        h1 "site 3"
       end
     end
   end
